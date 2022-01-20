a30152ffac7979e7c6b8dfe36fd0729dfd1d5253
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-01-20 21:14:11 +0200

    Add any? method in module.

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -12,3 +12,14 @@ module MyEnumerable
     end
     response
   end
+
+  def any?
+    return unless block_given?
+
+    response = false
+    list.each do |element|
+      response = true if yield element
+    end
+    response
+  end
+end
