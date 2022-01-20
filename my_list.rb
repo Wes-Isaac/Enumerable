c5c237f0519c97953e12dc5578e9461c8556b05b
unknown <yisehakmitiku@gmail.com>
2022-01-21 05:23:07 +0800

    Update based on required changes

--- my_list.rb
+++ my_list.rb
@@ -5,10 +5,18 @@ class MyList
   def initialize(*list)
     @list = list
   end
+
+  def each
+    i = @list.length
+    while i >= 0
+      puts @list[i]
+      i -= 1
+    end
+  end
 end
 
 list = MyList.new(1, 2, 3, 4)
-
+list.each
 list.all? { |e| e < 5 }
 list.all? { |e| e > 5 }
 list.any? { |e| e == 2 }
